21a43e0f2199b19749aa85f7b14c807d01e9a79d
masatoshi118 <masatworks118@gmail.com>
2020-06-05 16:14:06 +0900

    remove TurboLinks

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'mysql2', '>= 0.4.4'
 gem "puma", ">= 4.3.5"
 gem 'sass-rails', '>= 6'
 gem 'webpacker', '~> 4.0'
-gem 'turbolinks', '~> 5'
+# gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   get 'rooms/show'
   root to: 'events#index'
-  resources :users, only: [:show, :edit, :update]
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+  resources :users, only: [:show, :edit, :update]
   patch '/users/:id/user_update', to: 'users#user_update'
   resources :events do
     resource :joins, only: [:create, :destroy]
