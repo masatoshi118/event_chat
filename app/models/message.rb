1e2ee389e2a3e62525e99a4f078587032c27f9f3
masatoshi118 <masatworks118@gmail.com>
2020-06-17 12:03:00 +0900

    チャットルーム

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,8 @@ class Message < ApplicationRecord
   belongs_to :event
   validates :user_id, presence: true
   validates :event_id, presence: true
-  validates :content, presence: true
+  validates :content, presence: true,
+                      length: { maximum: 500}
 
   def template
     ApplicationController.render_with_signed_in_user(user, partial: 'messages/message', locals: { message: self })
